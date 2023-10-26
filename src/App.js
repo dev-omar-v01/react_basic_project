9c97dc888f60058b51afcbf5c9b7502f755b2c8b
softdevomar <omorfarukqqq@gmail.com>
2023-10-26 06:28:25 -0700

    react bootstrap added project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
 import React from "react";
-import List from "./components/list";
 import NestedList from "./components/nested_data_list";
 import Card1 from "./components/Card1";
 import Card2 from "./components/Card2";
 import Card3 from "./components/Card3";
-import './index.css'
 
 import { FaBeer } from 'react-icons/fa';
+import ReactBootstrap from "./react_bootstrap/ReactBootstrap";
 
 export default function App(){
     return (
@@ -15,9 +14,11 @@ export default function App(){
      
         {<NestedList />}
         {<Card1 />}
-        {<Card2 name="Md Omar Faruk" />}
+        {<Card2 name="Md Omar Farouk" />}
         {<Card3 />}
         {<FaBeer />}
+        {<ReactBootstrap />}
+        
     
         </>
     );
