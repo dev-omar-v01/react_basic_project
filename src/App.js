001229a0e1e9cce82bdd28bdc1ee3ea689f7a92b
softdevomar <omorfarukqqq@gmail.com>
2023-10-26 06:07:49 -0700

    react icon add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import NestedList from "./components/nested_data_list";
 import Card1 from "./components/Card1";
 import Card2 from "./components/Card2";
 import Card3 from "./components/Card3";
+import './index.css'
 
+import { FaBeer } from 'react-icons/fa';
 
 export default function App(){
     return (
@@ -15,6 +17,7 @@ export default function App(){
         {<Card1 />}
         {<Card2 name="Md Omar Faruk" />}
         {<Card3 />}
+        {<FaBeer />}
     
         </>
     );
